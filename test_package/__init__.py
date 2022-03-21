1db01e76c82338b4b4b59807114a9228ea8b7858
MXK606 <MXK606@alumni.bham.ac.uk>
2022-03-21 17:44:58 +0400

    Added docstring and same metadata as that for modules for the python package.

--- test_package/__init__.py
+++ test_package/__init__.py
@@ -1,1 +1,9 @@
-__all__ = ["index_calc", "spec_analysis", "plotting"]
+"""
+This is a Python package containing the modules; 'index_calc', 'spec_analysis' and 'plotting'.
+"""
+
+__author__ = "Mukul Kumar"
+__email__ = "Mukul.k@uaeu.ac.ae, MXK606@alumni.bham.ac.uk"
+__date__ = "21-03-2022"
+__version__ = "1.0"
+__all__ = ["index_calc", "spec_analysis", "plotting"] # Using this DUNDER function so that running 'from test_package import *' imports only these modules
