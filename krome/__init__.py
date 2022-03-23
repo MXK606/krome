71d955d7d774604e84cc02421eaf4e8ade0f6b0f
Mukul Kumar <47328060+MXK606@users.noreply.github.com>
2022-03-23 11:56:23 +0400

    Update date and version in __init__.py

--- krome/__init__.py
+++ krome/__init__.py
@@ -4,6 +4,6 @@ This is a Python package containing the modules; 'index_calc', 'spec_analysis' a
 
 __author__ = "Mukul Kumar"
 __email__ = "Mukul.k@uaeu.ac.ae, MXK606@alumni.bham.ac.uk"
-__date__ = "21-03-2022"
-__version__ = "1.0"
+__date__ = "23-03-2022"
+__version__ = "1.2"
 __all__ = ["index_calc", "spec_analysis", "plotting"] # Using this DUNDER function so that running 'from test_package import *' imports only these modules
